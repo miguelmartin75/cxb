1108e15a44ab21782dea4833405a98154c1f5afb
Miguel Martin <miguel@miguel-martin.com>
2025-07-09 20:59:54 -0700

    remove warnings

--- tests/c_api_test.cpp
+++ tests/c_api_test.cpp
@@ -7,10 +7,12 @@ CXB_C_EXPORT MString join_paths(StringSlice p1, StringSlice p2, Allocator* alloc
         alloc = &default_alloc;
     }
 
-    MString result{.data = nullptr, .len = 0, .null_term = true, .capacity = 0, .allocator = alloc};
+    MString result = MSTRING_CONSTRUCT(alloc);
     result.reserve(p1.len + p2.len + 1);
     result.extend(p1);
-    result.push_back('/');
+    if(result.back() != '/') {
+        result.push_back('/');
+    }
     result.extend(p2);
     return result;
 }
