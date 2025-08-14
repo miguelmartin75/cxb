2b0c3f2aece5eaaef3765284d6d6e2d1ab3063c7
Miguel Martin <miguel@miguel-martin.com>
2025-08-14 02:51:42 -0700

    Formatting

--- tests/test_arena.cpp
+++ tests/test_arena.cpp
@@ -16,9 +16,9 @@ TEST_CASE("push and pop", "[Arena]") {
 
     auto foos = arena_push_array<Foo>(arena, 10);
     REQUIRE(foos.len == 10);
-    REQUIRE((void*)(arena->start + arena->pos) == (void*)(foos.data + foos.len));
+    REQUIRE((void*) (arena->start + arena->pos) == (void*) (foos.data + foos.len));
     array_push_back(foos, arena, Foo{3, 5});
-    REQUIRE((void*)(arena->start + arena->pos) == (void*)(foos.data + foos.len));
+    REQUIRE((void*) (arena->start + arena->pos) == (void*) (foos.data + foos.len));
 
     REQUIRE(foos.len == 11);
     REQUIRE(foos.back().x == 3);
@@ -33,11 +33,11 @@ TEST_CASE("string push/pop", "[Arena]") {
     REQUIRE(arena->end - arena->start == KB(4));
 
     String8 str = arena_push_string8(arena);
-    REQUIRE((void*)(arena->start + arena->pos) == (void*)(str.data + str.n_bytes()));
+    REQUIRE((void*) (arena->start + arena->pos) == (void*) (str.data + str.n_bytes()));
     string8_push_back(str, arena, 'a');
-    REQUIRE((void*)(arena->start + arena->pos) == (void*)(str.data + str.n_bytes()));
+    REQUIRE((void*) (arena->start + arena->pos) == (void*) (str.data + str.n_bytes()));
     string8_insert(str, arena, 'b', 0);
-    REQUIRE((void*)(arena->start + arena->pos) == (void*)(str.data + str.n_bytes()));
+    REQUIRE((void*) (arena->start + arena->pos) == (void*) (str.data + str.n_bytes()));
     REQUIRE(str == S8_LIT("ba"));
     REQUIRE(str.n_bytes() == 3);
 }
@@ -47,15 +47,15 @@ TEST_CASE("string insert", "[Arena]") {
     REQUIRE(arena->end - arena->start == KB(4));
 
     String8 str = arena_push_string8(arena);
-    REQUIRE((void*)(arena->start + arena->pos) == (void*)(str.data + str.n_bytes()));
+    REQUIRE((void*) (arena->start + arena->pos) == (void*) (str.data + str.n_bytes()));
     string8_push_back(str, arena, 'a');
-    REQUIRE((void*)(arena->start + arena->pos) == (void*)(str.data + str.n_bytes()));
+    REQUIRE((void*) (arena->start + arena->pos) == (void*) (str.data + str.n_bytes()));
     string8_insert(str, arena, 'b', 0);
-    REQUIRE((void*)(arena->start + arena->pos) == (void*)(str.data + str.n_bytes()));
+    REQUIRE((void*) (arena->start + arena->pos) == (void*) (str.data + str.n_bytes()));
 
     REQUIRE(str == S8_LIT("ba"));
     REQUIRE(str.n_bytes() == 3);
-    REQUIRE((void*)(arena->start + arena->pos) == (void*)(str.data + str.n_bytes()));
+    REQUIRE((void*) (arena->start + arena->pos) == (void*) (str.data + str.n_bytes()));
 
     string8_pop_all(str, arena);
     REQUIRE(arena->pos == sizeof(Arena) + 0);
@@ -66,15 +66,15 @@ TEST_CASE("string extend", "[Arena]") {
     REQUIRE(arena->end - arena->start == KB(4));
 
     String8 str = arena_push_string8(arena, S8_LIT("abc"));
-    REQUIRE((void*)(arena->start + arena->pos) == (void*)(str.data + str.n_bytes()));
+    REQUIRE((void*) (arena->start + arena->pos) == (void*) (str.data + str.n_bytes()));
     string8_extend(str, arena, S8_LIT("def"));
-    REQUIRE((void*)(arena->start + arena->pos) == (void*)(str.data + str.n_bytes()));
+    REQUIRE((void*) (arena->start + arena->pos) == (void*) (str.data + str.n_bytes()));
 
     REQUIRE(str == S8_LIT("abcdef"));
     REQUIRE(str.n_bytes() == 7);
 
     string8_insert(str, arena, S8_LIT("middle"), 2);
-    REQUIRE((void*)(arena->start + arena->pos) == (void*)(str.data + str.n_bytes()));
+    REQUIRE((void*) (arena->start + arena->pos) == (void*) (str.data + str.n_bytes()));
     REQUIRE(str == S8_LIT("abmiddlecdef"));
     REQUIRE(str.n_bytes() == 13);
 
@@ -88,12 +88,11 @@ TEST_CASE("array insert", "[Arena]") {
 
     Array<int> xs = {};
     xs.push_back(arena, 10);
-    REQUIRE((void*)(arena->start + arena->pos) == (void*)(xs.data + xs.len));
+    REQUIRE((void*) (arena->start + arena->pos) == (void*) (xs.data + xs.len));
 
     int ys[] = {20, 30, 50, 80};
     xs.extend(arena, Array<int>(ys, 4));
-    REQUIRE((void*)(arena->start + arena->pos) == (void*)(xs.data + xs.len));
+    REQUIRE((void*) (arena->start + arena->pos) == (void*) (xs.data + xs.len));
 
     REQUIRE(xs.len == 5);
-
 }
