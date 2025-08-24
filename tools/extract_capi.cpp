33d6b96fc2416b31e331d936f0bfdaa596297127
Miguel Martin <miguel@miguel-martin.com>
2025-08-23 18:22:53 -0700

    Fix memory errors

--- tools/extract_capi.cpp
+++ tools/extract_capi.cpp
@@ -7,25 +7,21 @@ struct Foo {
 };
 
 void format_value(Arena* a, String8& dst, String8 args, Foo x) {
+    (void)args;
     dst.extend(a, S8_LIT("Foo(x="));
     format_value(a, dst, {}, x.x);
     dst.extend(a, S8_LIT(")"));
 }
 
-int fib(int n) {
-    if(n <= 1) return n;
-    int n1 = fib(n - 1);
-    int n2 = fib(n - 2);
-    println("fib({}) = fib({}) + fib({}) = {} + {} = {}", n, n - 1, n - 2, n1, n2, n1 + n2);
-    return n1 + n2;
-}
-
 int main(int argc, char* argv[]) {
+    (void)argc;
+    (void)argv;
+
     Foo f = {};
     f.x = -1.42;
-    // println("Hello world: {.12f}, {}, {}", 2.5, 2, f);
-    println("Hello world: {.2}", f.x);
-    println("Hello world: {}", false);
 
-    // fib(30);
+    println("Hello world, f= {}", f);
+    println("f.x={.2}", f.x);
+    println("bool: {}", false);
+    return 0;
 }
