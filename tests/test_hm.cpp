3a94777afaf5e21fe6cbca49ec344509c8703a91
Miguel Martin <miguel@miguel-martin.com>
2025-09-01 23:49:18 -0700

    base hashmap impl.

---
+++ tests/test_hm.cpp
@@ -0,0 +1,14 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_test_macros.hpp>
+#include <cxb/cxb.h>
+
+size_t hash(const int& x) {
+    return x; // TODO
+}
+
+TEST_CASE("basic", "[HashMap]") {
+    Arena* a = get_perm();
+    HashMap<int, int> kvs;
+    kvs.put(a, {1, 2});
+    REQUIRE(kvs.contains(1));
+};
