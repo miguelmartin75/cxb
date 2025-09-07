99d93407cfd5092537bd13e3024910a1dbb3b924
Miguel Martin <miguel@miguel-martin.com>
2025-09-06 23:57:09 -0700

    working functionality

--- tests/test_hm.cpp
+++ tests/test_hm.cpp
@@ -11,16 +11,35 @@ size_t hash(const int& x) {
 TEST_CASE("basic", "[HashMap]") {
     Arena* a = get_perm();
     HashMap<int, int> kvs;
-    kvs.put(a, {1, 2});
-    kvs.extend(a,
+    REQUIRE(kvs.put(a, {1, 2}));
+    for(auto& kv : kvs) {
+        REQUIRE(kv.key == 1);
+        REQUIRE(kv.value == 2);
+        break;
+    }
+
+    REQUIRE(kvs.len == 1);
+    REQUIRE(kvs.extend(a,
                {
                    {7, 9},
                    {3, 5},
                    {11, 9},
-               });
+               }));
     REQUIRE(kvs.contains(1));
     REQUIRE(kvs.contains(7));
     REQUIRE(kvs.contains(3));
     REQUIRE(kvs.contains(11));
     REQUIRE(!kvs.contains(2));
+    REQUIRE(kvs.len == 4);
+
+    REQUIRE(kvs[1] == 2);
+    REQUIRE(kvs.erase(1));
+    REQUIRE(kvs.len == 3);
+    REQUIRE(!kvs.erase(2));
+
+    REQUIRE(kvs.put(a, {1, 3}));
+    REQUIRE(kvs.len == 4);
+    REQUIRE(kvs.contains(1));
+
+    REQUIRE(kvs[1] == 3);
 };
