c7f297732cbd4c8097147723a039b72792f48ed7
Miguel Martin <miguel@miguel-martin.com>
2025-09-07 17:59:08 -0700

    WIP on extract_capi.cpp

--- scripts/extract_capi.cpp
+++ scripts/extract_capi.cpp
@@ -1,27 +1,28 @@
 #include <cxb/cxb.h>
-#include <fmt/format.h>
-#include <stdio.h>
 
-struct Foo {
-    double x;
-};
-
-void format_value(Arena* a, String8& dst, String8 args, Foo x) {
-    (void) args;
-    dst.extend(a, S8_LIT("Foo(x="));
-    format_value(a, dst, {}, x.x);
-    dst.extend(a, S8_LIT(")"));
-}
+#include "examples/memfile.h"
 
 int main(int argc, char* argv[]) {
-    (void) argc;
-    (void) argv;
+    (void)argc;
+    (void)argv;
 
-    Foo f = {};
-    f.x = -1.42;
+    String8 in = "../cxb/cxb-cxx.h"_s8;
+    String8 out = "../cxb/cxb-c.h"_s8;
 
-    println("Hello world, f= {}", f);
-    println("f.x={.2}", f.x);
-    println("bool: {}", false);
+    Arena* arena = get_perm();
+    auto in_f = open_file(arena, in);
+    if(in_f) {
+        println(stderr, "Error opening file {}, reason: {} (code={})", in, in_f.reason, (int)in_f.error);
+        return 1;
+    }
+    FILE* out_f = fopen(out.c_str(), "w");
+
+    String8 in_data_str = in_f.value.data.as_string8();
+    for(String8 line : in_data_str.split("\n"_s8)) {
+        if(line.startswith(
+    }
+
     return 0;
 }
+
+#include "examples/memfile.cpp"
